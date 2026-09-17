e39b21b029fc6c93a1a4504b318aae14c94ba7f0
utkarsh singh <f20251346@pilani.bits-pilani.ac.in>
2026-09-17 18:05:46 +0000

    lab02 task2

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -4,6 +4,10 @@
 //
 // YOU complete the two TODOs below. Everything else is given.
 
+// lut.v
+// A small parameterized ROM (lookup table): DEPTH words, each WIDTH bits
+// wide. dout continuously reflects mem[sel].
+
 module lut #(
   parameter WIDTH = 8,
   parameter DEPTH = 4
@@ -13,9 +17,21 @@ module lut #(
 );
 
   reg [WIDTH-1:0] mem [0:DEPTH-1];
-
   integer i;
 
+  // Initialize ROM contents
+  initial begin
+    for (i = 0; i < DEPTH; i = i + 1) begin
+      mem[i] = i * i;
+    end
+  end
+
+  // Combinational read
+  always @(*) begin
+    dout = mem[sel];
+  end
+
+endmodule
   // TODO: initialize mem[i] = i*i for every i from 0 to DEPTH-1.
   // Use an initial block with a for loop -- this is the only place a ROM's
   // contents should be set up. (See the lab manual for why.)
--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,14 +1,30 @@
 // tb.v
 // Starter testbench template -- YOU complete this file.
 
+// tb.v
+// Testbench for mux_beh
+
 module tb;
 
-  // TODO: declare the inputs and outputs
+  // Inputs to DUT
+  reg t_i0;
+  reg t_i1;
+  reg t_s;
 
-  // TODO: instantiate DUT here
+  // Output from DUT
+  wire t_y;
+
+  // Instantiate DUT
+  mux_beh DUT (
+    .I0(t_i0),
+    .I1(t_i1),
+    .S(t_s),
+    .Y(t_y)
+  );
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
+
   initial begin
     if ($value$plusargs("vcd=%s", vcd_file)) begin
       $dumpfile(vcd_file);
@@ -16,12 +32,41 @@ module tb;
     end
   end
 
+  // Apply different input combinations
   initial begin
-    // TODO: apply different input combinations
+    t_i0 = 0; t_i1 = 0; t_s = 0;
+    #10;
 
+    t_i0 = 0; t_i1 = 1; t_s = 0;
+    #10;
+
+    t_i0 = 0; t_i1 = 1; t_s = 1;
+    #10;
+
+    t_i0 = 1; t_i1 = 0; t_s = 0;
+    #10;
+
+    t_i0 = 1; t_i1 = 0; t_s = 1;
+    #10;
+
+    t_i0 = 1; t_i1 = 1; t_s = 0;
+    #10;
+
+    t_i0 = 1; t_i1 = 1; t_s = 1;
+    #10;
+
+    $finish;
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " I0=%b I1=%b S=%b | Y=%b",
+             t_i0, t_i1, t_s, t_y);
 
 endmodule
+
+  // TODO: declare the inputs and outputs
+
+  // TODO: instantiate DUT here
+
+  // Waveform dump configuration (DO NOT CHANGE)
+  
